a5db4dad5b04c94033a8e763395f7ce78b0be0f0
unknown <appowlok@gmail.com>
2023-03-31 19:39:05 +0530

    31 March Before invoice final

--- src/screens/Invoice/InvoiceDetail.js
+++ src/screens/Invoice/InvoiceDetail.js
@@ -268,7 +268,8 @@ import {COLORS} from '../../constants/Colors';
 import {FONTS} from '../../constants/themes';
 import BunchDealCommonBtn from '../../utils/BunchDealCommonBtn';
 import BunchDealProgressBar from '../../utils/BunchDealProgressBar';
-import RNFetchBlob from 'rn-fetch-blob';
+// import RNFetchBlob from 'rn-fetch-blob';
+import RNFetchBlob from 'react-native-fetch-blob';
 import {ShowConsoleLogMessage, ShowToastMessage} from '../../utils/Utility';
 const InvoiceDetail = ({navigation, route}) => {
   const [webViewLoading, setWebViewLoading] = useState(true);
@@ -276,84 +277,84 @@ const InvoiceDetail = ({navigation, route}) => {
   const showSpinner = () => setWebViewLoading(true);
   const hideSpinner = () => setWebViewLoading(false);
 
-  // const permissionFunc = async () => {
-  //   if (Platform.OS == 'ios') {
-  //     actualDownload();
-  //   } else {
-  //     if (downloaded) {
-  //       try {
-  //         const granted = await PermissionsAndroid.request(
-  //           PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-  //         );
-  //         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-  //           actualDownload();
-  //         } else {
-  //           ShowToastMessage(
-  //             'You need to give storage permission to download the file',
-  //           );
-  //         }
-  //       } catch (err) {
-  //         console.warn(err);
-  //       }
-  //     } else {
-  //       ShowToastMessage('File is already downloaded.');
-  //     }
-  //   }
-  // };
+  const permissionFunc = async () => {
+    if (Platform.OS == 'ios') {
+      actualDownload();
+    } else {
+      if (downloaded) {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            actualDownload();
+          } else {
+            ShowToastMessage(
+              'You need to give storage permission to download the file',
+            );
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      } else {
+        ShowToastMessage('File is already downloaded.');
+      }
+    }
+  };
 
-  // const actualDownload = () => {
-  //   const {dirs} = RNFetchBlob.fs;
-  //   const dirToSave =
-  //     Platform.OS == 'ios' ? dirs.DocumentDir : dirs.DownloadDir;
-  //   const configfb = {
-  //     useDownloadManager: true,
-  //     notification: true,
-  //     mediaScannable: true,
-  //     title: 'BOD_OrderID_' + order_id + '.pdf',
-  //     path: `${dirToSave}/BOD_OrderID_${order_id}.pdf`,
-  //     mime: 'application/pdf',
-  //   };
-  //   const configOptions = Platform.select({
-  //     ios: {
-  //       title: configfb.title,
-  //       path: configfb.path,
-  //       appendExt: 'pdf',
-  //     },
-  //     android: configfb,
-  //   });
+  const actualDownload = () => {
+    const {dirs} = RNFetchBlob.fs;
+    const dirToSave =
+      Platform.OS == 'ios' ? dirs.DocumentDir : dirs.DownloadDir;
+    const configfb = {
+      useDownloadManager: true,
+      notification: true,
+      mediaScannable: true,
+      title: 'BOD_OrderID_' + order_id + '.pdf',
+      path: `${dirToSave}/BOD_OrderID_${order_id}.pdf`,
+      mime: 'application/pdf',
+    };
+    const configOptions = Platform.select({
+      ios: {
+        title: configfb.title,
+        path: configfb.path,
+        appendExt: 'pdf',
+      },
+      android: configfb,
+    });
 
-  //   ShowConsoleLogMessage(
-  //     'The file saved to 23233 -> ' +
-  //       JSON.stringify(configfb) +
-  //       ' ' +
-  //       JSON.stringify(dirs),
-  //   );
+    ShowConsoleLogMessage(
+      'The file saved to 23233 -> ' +
+        JSON.stringify(configfb) +
+        ' ' +
+        JSON.stringify(dirs),
+    );
 
-  //   RNFetchBlob.config(configOptions)
-  //     .fetch('GET', `${url}`, {})
-  //     .then(res => {
-  //       if (Platform.OS === 'ios') {
-  //         RNFetchBlob.ios.previewDocument(configfb.path);
-  //       }
-  //       if (Platform.OS === 'android') {
-  //         RNFetchBlob.android.actionViewIntent(
-  //           res.path(),
-  //           configOptions.mime || 'application/pdf',
-  //         );
-  //       }
+    RNFetchBlob.config(configOptions)
+      .fetch('GET', `${url}`, {})
+      .then(res => {
+        if (Platform.OS === 'ios') {
+          RNFetchBlob.ios.previewDocument(configfb.path);
+        }
+        if (Platform.OS === 'android') {
+          RNFetchBlob.android.actionViewIntent(
+            res.path(),
+            configOptions.mime || 'application/pdf',
+          );
+        }
 
-  //       setisdownloaded(false);
-  //       if (Platform.OS == 'android') {
-  //         ShowToastMessage('File downloaded');
-  //       }
-  //       ShowConsoleLogMessage('The file saved to ' + JSON.stringify(res));
-  //     })
-  //     .catch(e => {
-  //       setisdownloaded(true);
-  //       ShowToastMessage(e.message);
-  //       ShowConsoleLogMessage('The file saved to ERROR ' + e.message);
-  //     });
-  // };
+        setisdownloaded(false);
+        if (Platform.OS == 'android') {
+          ShowToastMessage('File downloaded');
+        }
+        ShowConsoleLogMessage('The file saved to ' + JSON.stringify(res));
+      })
+      .catch(e => {
+        setisdownloaded(true);
+        ShowToastMessage(e.message);
+        ShowConsoleLogMessage('The file saved to ERROR ' + e.message);
+      });
+  };
 
   let {url} = route?.params;
   let {order_id} = route?.params;
@@ -419,7 +420,8 @@ const InvoiceDetail = ({navigation, route}) => {
         textStyle={FONTS.body3}
         textColor={COLORS.white}
         onPress={() => {
-          // permissionFunc();
+          //permissionFunc();
+          ShowToastMessage('Work in progress!');
         }}
         borderRadius={1}
         textSize={16}
